6d669e8e6d4e3edd7be7d51574b43f62391775fd
Muhammadnabi <xoliqulovmuhammadnabi@gmail.com>
2025-12-01 03:51:54 +0500

    mini changes

--- app/Filament/Widgets/MyCalendarWidget.php
+++ app/Filament/Widgets/MyCalendarWidget.php
@@ -6,42 +6,154 @@ use App\Models\MyEvent;
 use Filament\Actions\CreateAction;
 use Filament\Forms\Components\DateTimePicker;
 use Filament\Forms\Components\TextInput;
-// use Filament\Widgets\Widget;
+use Guava\Calendar\Enums\CalendarViewType;
+use Guava\Calendar\Filament\Actions\EditAction;
+use Guava\Calendar\Filament\Actions\ViewAction;
 use Guava\Calendar\Filament\CalendarWidget;
-use Guava\Calendar\ValueObjects\CalendarEvent;
 use Guava\Calendar\ValueObjects\FetchInfo;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Log;
 
 class MyCalendarWidget extends CalendarWidget
 {
+    // protected CalendarViewType $calendarView = CalendarViewType::ListWeek;
+
     // protected CalendarViewType $calendarView = CalendarViewType::ResourceTimeGridWeek;
+
+    protected CalendarViewType $calendarView = CalendarViewType::DayGridMonth;
+
+    protected bool $dayMaxEvents = true;
+
+    protected bool $dateClickEnabled = true;
+    protected bool $eventClickEnabled = true;
+
+    protected bool $dateSelectEnabled = true;
+
+    protected function getEventClickContextMenuActions(): array
+    {
+        return [
+            $this->viewAction(),
+            $this->editAction(),
+            // $this->deleteAction(),
+        ];
+    }
+
+
+
+    protected function getDateClickContextMenuActions(): array
+    {
+        return [
+            CreateAction::make('create')
+                ->model(MyEvent::class)
+                ->mountUsing(fn($arguments, $form) => $form->fill([
+                    'title' => null,
+                    'start' => data_get($arguments, 'dateStr'),
+                    'end'   => data_get($arguments, 'dateStr'),
+                ]))
+                ->schema([
+                    TextInput::make('title')
+                        ->required(),
+                    DateTimePicker::make('start')
+                        ->required(),
+                    DateTimePicker::make('end')
+                        ->required(),
+                ])
+                ->after(fn() => $this->refreshRecords())
+        ];
+    }
+    public function editAction(): EditAction
+    {
+        return EditAction::make('edit')
+            ->modal(true)
+
+            ->record(function ($arguments) {
+                $key = data_get($arguments, 'data.event.extendedProps.key');
+                $model = data_get($arguments, 'data.event.extendedProps.model');
+
+                if ($key && $model && class_exists($model)) {
+                    return $model::find($key);
+                }
+
+                $id = data_get($arguments, 'data.event.id');
+                if (is_string($id) && preg_match('/\{generated-(\d+)\}/', $id, $m)) {
+                    $id = $m[1];
+                }
+
+                if ($id) {
+                    return MyEvent::find($id);
+                }
+
+                throw new \RuntimeException('Event record not found for EDIT');
+            })
+
+            ->schema([
+                TextInput::make('title')->required(),
+                DateTimePicker::make('start')->required(),
+                DateTimePicker::make('end')->required(),
+            ])
+
+            ->after(fn() => $this->refreshRecords());
+    }
     public function getHeaderActions(): array
     {
         return [
             CreateAction::make()
-            ->label('Create Event')
-            ->modal(true)
-            ->model(MyEvent::class)
-            ->schema([
-                TextInput::make('title')
-                ->required(),
-                DateTimePicker::make('start')
-                ->required(),
-                DateTimePicker::make('end')
-                ->required(),
-            ])
-            ->action(function(array $data){
-                MyEvent::create([
-                    'title'=>$data['title'],
-                    'start'=>$data['start'],
-                    'end'=>$data['end'],
-                ]);
-                $this->refreshRecords();
-            })
+                ->label('Create Event')
+                ->modal(true)
+                ->model(MyEvent::class)
+                ->schema([
+                    TextInput::make('title')
+                        ->required(),
+                    DateTimePicker::make('start')
+                        ->required(),
+                    DateTimePicker::make('end')
+                        ->required(),
+                ])
+                ->action(function (array $data) {
+                    MyEvent::create([
+                        'title' => $data['title'],
+                        'start' => $data['start'],
+                        'end' => $data['end'],
+                    ]);
+                    $this->refreshRecords();
+                })
         ];
     }
     protected function getEvents(FetchInfo $info): Collection|array
     {
         return MyEvent::get();
+    }
+    public function viewAction(): ViewAction
+    {
+        return ViewAction::make()
+            ->label('View')
+            ->modal(true)
+            ->record(function ($arguments) {
+                $key = data_get($arguments, 'data.event.extendedProps.key');
+                $model = data_get($arguments, 'data.event.extendedProps.model');
+
+                if ($key && $model && class_exists($model)) {
+                    return $model::find($key);
+                }
+
+                $id = data_get($arguments, 'data.event.id');
+                if (is_string($id) && preg_match('/\{generated-(\d+)\}/', $id, $m)) {
+                    $id = $m[1]; // "3"
+                }
+                if ($id) {
+                    return MyEvent::find($id);
+                }
+                throw new \RuntimeException('Event record not found');
+            })
+            ->schema([
+                TextInput::make('title')->required(),
+                DateTimePicker::make('start')->required(),
+                DateTimePicker::make('end')->required(),
+            ])
+            ->after(fn() => $this->refreshRecords());
+    }
+    public function authorize($ability, $arguments = [])
+    {
+        return true;
     }
 }
